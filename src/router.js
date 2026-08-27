5d9ff31e06175ff943a5133f9919950ed306bb64
ZJH917 <875520425@qq.com>
2026-08-27 11:09:11 +0800

    Initial upload: PlanLog website

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('./views/HomeView.vue')
+  },
+  {
+    path: '/detail/:workId',
+    name: 'detail',
+    component: () => import('./views/DetailView.vue')
+  },
+  {
+    path: '/timeline/:workId',
+    name: 'timeline',
+    component: () => import('./views/TimelineView.vue')
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
