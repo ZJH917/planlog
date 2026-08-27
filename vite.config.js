931aaca595a4a6671b23125401f49c05040dad86
ZJH917 <875520425@qq.com>
2026-08-27 11:19:25 +0800

    Update: Vite 6 + build config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 0
+  },
   server: {
     port: 5176,
     proxy: {
